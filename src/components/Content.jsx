4c8b533c26f5552b448bf6745869a87028b33f75
May Adnan <j_may96@hotmail.com>
2025-02-12 13:42:12 +0100

    Fix GH Pages deployment

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,11 +8,7 @@ const Content = () => {
     <main className="hero d-flex flex-column justify-content-center align-items-center text-center">
       <div id="content">
         <img src="assets/may.JPG" alt="May Adnan" className="profile-img" />
-        <img
-          src="/assets/workmay.JPG"
-          alt="May Adnan"
-          className="profile-img"
-        />
+        <img src="assets/workmay.JPG" alt="May Adnan" className="profile-img" />
         <p className="lead text-secondary">.NET Developer</p>
         <p className="intro" style={{ maxWidth: "600px" }}>
           Passionate and solution-oriented .NET Developer with expertise in C#,
